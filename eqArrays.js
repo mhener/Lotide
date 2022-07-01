54ce42ea73914bc1d46ab7e0ecaa5a721ca505b6
Mardin Hener <mardinhener14@gmail.com>
2022-07-01 19:22:50 +0000

    eslint and cleaning up

--- eqArrays.js
+++ eqArrays.js
@@ -7,19 +7,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let result = false;
   if (array1.length !== array2.length) {
     return false;
   }
   if (array1.length === array2.length) {
-     for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-          return false;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
       }
-     }
-     }
-     return true;
-    };
+    }
+  }
+  return true;
+};
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false

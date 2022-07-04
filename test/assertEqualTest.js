cd16d7e3cd042dd793b12f1453d064edf6f35ec5
Mardin Hener <mardinhener14@gmail.com>
2022-07-04 18:31:33 +0000

    eslint final version

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual('lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(4, 5);
+assertEqual('Mardin', 'Hana');
+assertEqual('Mardin', 'Mardin');
